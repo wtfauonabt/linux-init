065e9a7807c7f3633ccc94a6e60fcec387523463
Ubuntu <ubuntu@ip-172-31-44-117.ap-east-1.compute.internal>
2022-12-18 19:57:45 +0000

    Updated script with install commands for docker

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -10,3 +10,11 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# perform install
+sudo apt-get update -y
+sudo apt-get install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-compose-plugin
